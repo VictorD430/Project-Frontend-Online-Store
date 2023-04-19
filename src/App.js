ca3411ae3c12be17c6bd32b59a2bcb2dd2e864c8
Jaine Oliveira <jaine.nayara.23@gmail.com>
2023-04-19 18:05:47 -0300

    Requisito 3 ok - adicionando rota no app e criando page do shopping cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomeInitialMessage from './pages/HomeInitialMessage';
 import './App.css';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={ HomeInitialMessage } />
+        <Route path="/ShoppingCart" component={ ShoppingCart } />
+      </Switch>
 
-    <HomeInitialMessage />
+    </div>
 
   );
 }
