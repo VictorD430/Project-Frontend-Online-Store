5a0600568f6e3e6d0cd5e51e2be19ab5487fc5b3
Talia Fretta <frettatalia@gmail.com>
2023-04-19 18:38:58 -0300

    requisito 4 ok

--- src/pages/HomeInitialMessage.jsx
+++ src/pages/HomeInitialMessage.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class HomeInitialMessage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriasList: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.listagem();
+  }
+
+  listagem = async () => {
+    const categoriasList = await getCategories();
+    this.setState({
+      categoriasList,
+    });
+  };
+
   render() {
+    const {
+      categoriasList,
+    } = this.state;
     return (
 
       <main>
@@ -19,6 +41,22 @@ class HomeInitialMessage extends Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <aside className="categorias-list">
+          <h3>Categorias:</h3>
+          {categoriasList.map(({ id, name }) => (
+            <div key={ id }>
+              <button
+                type="button"
+                data-testid="category"
+                id={ id }
+                onClick={ this.handleClick }
+              >
+                {name}
+              </button>
+            </div>
+          ))}
+        </aside>
+
       </main>
     );
   }
